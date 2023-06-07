fa8c45c35bf96ce783e07b8f8ef7a20dcb629ae9
MrStijket <mr.stijket@gmail.com>
2023-06-07 23:42:10 +0300

    Read, Write Sync and ASync

---
+++ 4-names.js
@@ -0,0 +1,7 @@
+// local
+const secret = 'Secret File';
+// share
+const john = 'john';
+const peter = 'peter';
+
+module.exports = { john, peter };
---
+++ 7-mind-grenade.js
@@ -0,0 +1,7 @@
+const num1 = 5;
+const num2 = 10;
+function addValues() {
+  console.log(`the result:${num1 + num2}`);
+}
+
+addValues();
